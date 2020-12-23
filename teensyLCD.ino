45443f5d61d087d60ae289a49b87febe59c527fc
cy <cycheung96@gmail.com>
2020-12-22 18:27:13 -0600

    Initial commit

---
+++ teensyLCD.ino
@@ -0,0 +1,30 @@
+#define NUMROWS 4
+#define NUMCOLS 20
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
+// Moves cursor to input row and column
+void moveCursor(int row, int col) {
+
+}
+
+// Writes input C string of length len to row and column
+void writeCString(char* inputString, int len, int row, int col) {
+
+}
+
+// Writes input character to row and column on screen
+void writeChar(char inputChar, int row, int col) {
+
+}
+
+// Clears the entire screen
+void clearScreen() {
+
+}
