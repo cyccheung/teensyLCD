eaa51ba6d32cb5e0769e4a071bc412b65dd62531
cy <cycheung96@gmail.com>
2020-12-31 18:17:16 -0600

    Wrote up new FSM that differentiates cleanly where things are done in code

--- teensyLCD.ino
+++ teensyLCD.ino
@@ -7,6 +7,7 @@
 #define NUMCOLS 20              // LCD width
 #define NUMMENUITEMS 7          // Number of parameters and options
 #define MENUITEMMAXLENGTH 16    // Length of longest parameter name including null character
+#define PRINTDELAY 50           // ms to wait after printing
 #define SETUPDEBUG 0            // Does not run loop if true
 #define EEPROMENABLE 0          // Disable at all times unless testing EEPROM or overall system
 // EEPROM addresses
@@ -59,8 +60,16 @@ float resolutions[] = { 0.1,  // Step size
 char selector = '>';  // Symbol to use for selecting parameter in main menu
 
 enum STATES {
-  SCROLLING,  // Choosing which parameter to edit
-  ADJUSTING   // Editing a parameter's value
+  // Editing a parameter's value
+  ADJSTEPSIZE,
+  ADJNUMSTEPS,
+  ADJTOTALTRAVEL,
+  ADJEXPTIME,
+  ADJSCANDIR,
+  ADJPIEZOTRAVEL,
+  ADJSAVESETTINGS,  // State not used, for completeness only
+  // Scrolling in menu
+  SCROLLING
 };
 
 enum MENUITEMS {
@@ -73,10 +82,9 @@ enum MENUITEMS {
   SAVESETTINGS
 };
 
-int state = SCROLLING;
+int state = SCROLLING;                // Start off in scrolling mode
 int currentSelection = STEPSIZE;      // Stores which menu item currently pointing at
 int expTimePosition = 0;              // Stores offset from start of exposure time value cells
-int eepromAddr = 0;
 
 void loadSettings();
 void printPage(int currentSelection);
@@ -114,30 +122,70 @@ void loop() {
 
   // Check to see if the button has been pressed
   // Passes in a debounce delay of 10 milliseconds
+  // This block of code deals with state machine logic only, nothing should be printed
+  // and cursor should not be moved here, only state transitions, global variable updates, and cursor blink
   if (r.buttonPressedReleased(10)) {
-    if(state == SCROLLING) {
-      state = ADJUSTING;
-      if(currentSelection == EXPTIME) {
-
-      }
-      else {
-        // Move cursor to right most spot and blink it
-        lcd.setCursor(NUMCOLS - 1, currentSelection);
-      }
-      lcd.blink();
-    }
-    // Special case if user is changing exposure time
-    // Move the cursor to the right by one spot
-    else if(state == ADJUSTING && currentSelection == EXPTIME && expTimePosition < 3) {
-      expTimePosition++;
-      lcd.setCursor(NUMCOLS - 1 - parameterValueLengths[currentSelection] + expTimePosition, currentSelection);
-    }
-    // User is done adjusting value
-    else if(state == ADJUSTING) {
-      expTimePosition = 0;  // Redundant most of the time but makes the code simpler so...
-      state = SCROLLING;
-      lcd.setCursor(0, currentSelection);
-      lcd.noBlink();
+    switch(state) {
+      case SCROLLING:
+        // If selector is pointing at save settings, call save function
+        if(currentSelection == SAVESETTINGS) {
+          if(EEPROMENABLE) {
+            saveSettings();
+          }
+        }
+        // If selector pointing at parameters
+        else {
+          // If user is scrolling, send him to adjusting state for chosen parameter
+          state = currentSelection;
+        }
+        lcd.blink();
+        break;
+      case ADJSTEPSIZE:
+        // User is done adjusting step size, just go back to scrolling
+        state = SCROLLING;
+        lcd.noBlink();
+        break;
+      case ADJNUMSTEPS:
+        // User is done adjusting num steps, just go back to scrolling
+        state = SCROLLING;
+        lcd.noBlink();
+        break;
+      case ADJTOTALTRAVEL:
+        // User is done adjusting total travel, just go back to scrolling
+        state = SCROLLING;
+        lcd.noBlink();
+        break;
+      case ADJEXPTIME:
+        // User is done editing current digit
+        // If user still has digits to edit, increment expTimePosition and keep him in ADJEXPTIME state
+        if(expTimePosition < 3) {
+          state = ADJEXPTIME;
+          expTimePosition++;
+        }
+        // If user is done editing the ones place digit, send him back to scrolling
+        else {
+          state = SCROLLING;
+          lcd.noBlink();
+          expTimePosition = 0;
+        }
+        break;
+      case ADJSCANDIR:
+        // User is done adjusting scan direction, just go back to scrolling
+        state = SCROLLING;
+        lcd.noBlink();
+        break;
+      case ADJPIEZOTRAVEL:
+        // User is done adjusting piezo travel, just go back to scrolling
+        state = SCROLLING;
+        lcd.noBlink();
+        break;
+      case ADJSAVESETTINGS:
+        // Should never be in this state, state exists for completeness only
+        state = SCROLLING;
+        lcd.noBlink();
+        break;
+      default:
+        break;
     }
   }
 
@@ -145,109 +193,76 @@ void loop() {
   if(val) {
     switch(state) {
       case SCROLLING:
-        lcd.setCursor(0, currentSelection % 4); // Move cursor to 0th column and chosen row
-        lcd.noAutoscroll();                 // Left justify
-        lcd.print(" ");                     // Erase selector symbol
-        delay(50);
+        lcd.setCursor(0, currentSelection % 4); // Move cursor to selector
+        lcd.noAutoscroll();                     // Left justify
+        lcd.print(" ");                         // Erase selector symbol
+        delay(PRINTDELAY);
 
-        // Change which row selector symbol is on and saturate it between 0 and NUMMENUITEMS - 1
-        if(val == r.clockwise() && currentSelection == 3) {
-          currentSelection >= NUMMENUITEMS - 1 ? currentSelection = NUMMENUITEMS - 1 : currentSelection++;
-          // Print page 2
+        // If going from page 1 to page 2
+        if(val == r.clockwise() && currentSelection == NUMROWS - 1) {
+          currentSelection++;
           printPage(currentSelection);
         }
-        else if(val == r.clockwise()) {
-          currentSelection >= NUMMENUITEMS - 1 ? currentSelection = NUMMENUITEMS - 1 : currentSelection++;
-        }
-        else if(val == r.counterClockwise() && currentSelection == 4) {
-          currentSelection <= 0 ? currentSelection = 0 : currentSelection--;
-          // Print page 1
+        // If going from page 2 to page 1
+        else if(val == r.counterClockwise() && currentSelection == NUMROWS) {
+          currentSelection--;
           printPage(currentSelection);
         }
-        else if(val == r.counterClockwise()) {
-          currentSelection <= 0 ? currentSelection = 0 : currentSelection--;
+        // If within one page
+        else {
+          val == r.clockwise() ? currentSelection++ : currentSelection--;
         }
+        // Saturate currentSelection
+        currentSelection = saturate(currentSelection, 0, NUMMENUITEMS - 1)
 
         lcd.setCursor(0, currentSelection % 4); // Move cursor to 0th column and chosen row
-        lcd.noAutoscroll();                 // Left justify
-        lcd.print(selector);                // Print out selector symbol
-        delay(50);
+        lcd.noAutoscroll();                     // Left justify
+        lcd.print(selector);                    // Print out selector symbol
+        delay(PRINTDELAY);
         break;
-      case ADJUSTING:
-        switch(currentSelection) {
-          // case STEPSIZE:
-          //   break;
-          // case NUMSTEPS:
-          //   break;
-          // case TOTALTRAVEL:
-          //   break;
-          case EXPTIME:
-            // Lets user adjust exposure time like a luggage lock
-            int difference = 1000;
-            for(int i = 0; i < expTimePosition; ++i) {
-              difference /= 10;
-            }
-//            lcd.setCursor();
-            if(val == r.clockwise()) {
-              values[currentSelection] += difference;                       // Increment by difference
-              if(values[currentSelection] >= valuesMax[currentSelection]) {
-                values[currentSelection] = valuesMax[currentSelection];     // Saturate at max
-              } 
-            }
-            else if(val == r.counterClockwise()) {
-              values[currentSelection] -= difference;                       // Decrement by difference
-              if(values[currentSelection] <= valuesMin[currentSelection]) {
-                values[currentSelection] = valuesMin[currentSelection];     // Saturate at min
-              }
-            }
-            updateValueOnScreen(currentSelection);
-            break;
-          case SCANDIR:
-            if(values[currentSelection] < 0.5) {
-              values[currentSelection] = 1.0;
-            }
-            else {
-              values[currentSelection] = 0.0;
-            }
-            lcd.setCursor(NUMCOLS - 1 - parameterValueLengths[currentSelection], currentSelection); // Move cursor to last column and chosen row
-            values[currentSelection] < 0.5 ? lcd.print("-") : lcd.print("+"); // 0.0 = -, 1.0 = +
-            delay(50);
-            break;
-          // case PIEZOTRAVEL:
-          //   break;
-          case SAVESETTINGS:
-            if(EEPROMENABLE) {
-              saveSettings();
-            }
-            break;
-          // Assume that all parameters other than SCANDIR has resolution and saturation values
-          default:
-            if(val == r.clockwise()) {
-              values[currentSelection] >= valuesMax[currentSelection] ? 
-                values[currentSelection] = valuesMax[currentSelection] :    // Saturate at max
-                values[currentSelection] += resolutions[currentSelection];  // Increment by resolution
-            }
-            else if(val == r.counterClockwise()) {
-              values[currentSelection] <= valuesMin[currentSelection] ? 
-                values[currentSelection] = valuesMin[currentSelection] :    // Saturate at min
-                values[currentSelection] -= resolutions[currentSelection];  // Decrement by resolution
-            }
-            updateValueOnScreen(currentSelection);
-            // lcd.setCursor(NUMCOLS - 1 - parameterValueLengths[currentSelection], currentSelection); // Move cursor to correct column from the right and chosen row
-            // lcd.print(values[currentSelection]);          // Print out value
-            // delay(50);
-            // lcd.print(parameterUnits[currentSelection]);  // Print out units
-            // delay(50);
-            // lcd.setCursor(NUMCOLS - 1, currentSelection); // Move cursor back to last column on row so blinking is in the right place
-            break;
+      case ADJSTEPSIZE:
+        val == r.clockwise() ? values[currentSelection] += 0.1 : values[currentSelection] -= 0.1;
+        values[currentSelection] = saturate(values[currentSelection], 0.0f, 10.0f); // TODO: Replace limits with calculated limits
+        printValue(currentSelection);
+        // TODO: Update value of total travel and print it out
+        break;
+      case ADJNUMSTEPS:
+        val == r.clockwise() ? values[currentSelection] += 1 : values[currentSelection] -= 1;
+        values[currentSelection] = saturate(values[currentSelection], 0.0f, 10.0f); // TODO: Replace limits with calculated limits
+        printValue(currentSelection);
+        // TODO: Update value of total travel and print it out
+        break;
+      case ADJTOTALTRAVEL:
+        // Should not be adjusting this value
+        break;
+      case ADJEXPTIME:
+        // Logic edits thousands place first
+        int difference = 1000;
+        for(int i = 0; i < expTimePosition; ++i) {
+          difference /= 10;
         }
+        val == r.clockwise() ? values[currentSelection] += difference : values[currentSelection] -= difference;
+        values[currentSelection] = saturate(values[currentSelection], 0.0f, 9999.0f);
+        printValue(currentSelection);
+        lcd.setCursor(NUMCOLS - 1 - 5 + expTimePosition);   // So cursor blinks over digit being edited
+        break;
+      case ADJSCANDIR:
+      // Toggle between 0.0 and 1.0
+        values[currentSelection] < 0.5 ? values[currentSelection] = 1.0 : values[currentSelection] = 0.0;
+        break;
+      case ADJPIEZOTRAVEL:
+        val == r.clockwise() ? values[currentSelection] += 50.0 : values[currentSelection] -= 50.0;
+        values[currentSelection] = saturate(values[currentSelection], 100.0f, 500.0f);
+        printValue(currentSelection);
+        break;
+      case ADJSAVESETTINGS:
         break;
       default:
         break;
     }
   }
 }
-
+/*
 // Function to update value of parameter on screen
 void updateValueOnScreen(int currentSelection) {
   if(currentSelection == SAVESETTINGS) { return; }
@@ -289,7 +304,7 @@ void updateValueOnScreen(int currentSelection) {
     lcd.setCursor(NUMCOLS - 1, currentSelection); 
   }
 }
-
+*/
 // Function to print out menu labels
 // Takes in position of menu selector
 void printMenuLabels(int currentSelection) {
@@ -298,6 +313,7 @@ void printMenuLabels(int currentSelection) {
     for(int i = 4; i < 7; ++i) {
       lcd.setCursor(0, i - 4);
       lcd.print(menuLabels[i]);
+      delay(PRINTDELAY);
     }
   }
   // Page 1
@@ -305,26 +321,70 @@ void printMenuLabels(int currentSelection) {
     for(int i = 0; i < 4; ++i) {
       lcd.setCursor(0, i);
       lcd.print(menuLabels[i]);
+      delay(PRINTDELAY);
     }
   }
 }
 
-// Function to print out parameter values
+// Function to print out parameter value
 // Takes in position of menu selector
-void printValues(int currentSelection) {
-  // Page 2
-  if(currentSelection >= 4) {
-    for(int i = 4; i < 6; ++i) {
-      lcd.setCursor(0, i - 4);
-      lcd.print(values[i]);
-    }
-  }
-  // Page 1
-  else {
-    for(int i = 0; i < 4; ++i) {
-      lcd.setCursor(0, i);
-      lcd.print(values[i]);
-    }
+// Does not move cursor to where it should be next, let another function handle
+void printValue(int currentSelection) {
+  int printRow = currentSelection % 4;
+  switch(currentSelection) {
+    case STEPSIZE:
+      values[currentSelection] >= 10 ? lcd.setCursor(NUMCOLS - 1 - 6, printRow) : lcd.setCursor(NUMCOLS - 1 - 5, printRow);
+      lcd.print(values[currentSelection]);
+      delay(PRINTDELAY);
+      lcd.print("um");
+      delay(PRINTDELAY);
+      break;
+    case NUMSTEPS:
+      // TODO: See how many digits are needed
+      lcd.setCursor(NUMCOLS - 1 - 6, printRow);
+      lcd.print((int)values[currentSelection]);   // Cast to an int
+      delay(PRINTDELAY);
+      break;
+    case TOTALTRAVEL:
+      // TODO: See how many digits are needed
+      lcd.setCursor(NUMCOLS - 1 - 6, printRow);
+      lcd.print(values[currentSelection]);
+      delay(PRINTDELAY);
+      lcd.print("um");
+      delay(PRINTDELAY);
+      break;
+    case EXPTIME:
+      lcd.setCursor(NUMCOLS - 1 - 5, printRow);
+      if(values[currentSelection] < 1000) lcd.print("0"); // Print leading zeros
+      if(values[currentSelection] < 100) lcd.print("0");
+      if(values[currentSelection] < 10) lcd.print("0"); 
+      lcd.print((int)values[currentSelection]);
+      delay(PRINTDELAY);
+      lcd.print("ms");
+      delay(PRINTDELAY);
+      break;
+    case SCANDIR:
+      lcd.setCursor(NUMCOLS - 1, printRow);
+      if(values[currentSelection] > 0.5) {
+        lcd.print("+");
+      }
+      else {
+        lcd.print("-");
+      }
+      delay(PRINTDELAY);
+      break;
+    case PIEZOTRAVEL:
+      lcd.setCursor(NUMCOLS - 1 - 4, printRow);
+      lcd.print(values[currentSelection]);
+      delay(PRINTDELAY);
+      lcd.print("um");
+      delay(PRINTDELAY);
+      break;
+    case SAVESETTINGS:
+      // No value to print
+      break;
+    default:
+      break;
   }
 }
 
@@ -333,8 +393,17 @@ void printValues(int currentSelection) {
 void printPage(int currentSelection) {
   lcd.clear();
   printMenuLabels(currentSelection);
-  updateValueOnScreen(currentSelection);
-//  printValues(currentSelection);
+  if(currentSelection < 4) {
+    for(int i = 0; i < 4; ++i) {
+      printValue(i);
+    }
+  }
+  else {
+    for(int i = 4; i < 6; ++i) {
+      printValue(i);
+    }
+  }
+  // updateValueOnScreen(currentSelection);
 }
 
 // Compute resolution, min, and max values for following parameters:
@@ -362,4 +431,16 @@ void loadSettings() {
   EEPROM.get(EEPROMEXPTIMEADDR, values[EXPTIME]);
   EEPROM.get(EEPROMSCANDIRADDR, values[SCANDIR]);
   EEPROM.get(EEPROMPIEZOTRAVELADDR, values[PIEZOTRAVEL]);
+}
+
+// Function that saturates val between min and max
+float saturate(float val, float min, float max) {
+  val < min ? return min :
+  val > max ? return max :
+  return val;
+}
+int saturate(int val, int min, int max) {
+  val < min ? return min :
+  val > max ? return max :
+  return val;
 }
